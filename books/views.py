480f8289a1a80c869446953c5545669aee5a8063
Psychox1k <success01001011@gmail.com>
2026-01-24 16:29:53 +0100

    feat(books): create custom permission IsAdminOrReadOnly and implement it in views

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import serializers, viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer
 
 
@@ -9,3 +11,5 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAdminOrReadOnly,)
