5e51a508975f8173bd9166ecc1773454d9ea0cb9
Psychox1k <success01001011@gmail.com>
2026-01-24 14:26:02 +0100

    feat: add django-debug-toolbar to library_service

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -18,7 +18,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from library_service_api import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
