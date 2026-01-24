8aab6f5c71dd7f1076a32b0493046f8a5853b6d9
Psychox1k <success01001011@gmail.com>
2026-01-24 13:43:12 +0100

    feat(books): create views for Book model

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import serializers, viewsets
+
+from books.models import Book
+from books.serializers import BookSerializer
+
 
 # Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
