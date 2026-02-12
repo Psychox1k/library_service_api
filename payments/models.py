33e9b5e48a1dc8eb6569b1a94981596f2dfd2654
Psychox1k <success01001011@gmail.com>
2026-02-12 21:35:53 +0100

    feat(payment): implement and register Payment model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from borrowings.models import Borrowing
+
 
 # Create your models here.
+class Payment(models.Model):
+
+    class StatusType(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+    class Type(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(max_length=7, choices=StatusType.choices)
+    type = models.CharField(max_length=7, choices=Type.choices)
+
+    borrowing = models.ForeignKey(
+        Borrowing,
+        on_delete=models.CASCADE,
+        related_name="payments"
+    )
+
+    session_url = models.URLField(max_length=500, blank=True, null=True)
+    session_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
+
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return (f"{self.get_type_display()} for Borrowing"
+                f" {self.borrowing.id} ({self.get_status_display()}")
