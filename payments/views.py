42aa9e46da8cea8c36d081804cc374a2a86388c4
Psychox1k <success01001011@gmail.com>
2026-02-12 21:37:41 +0100

    feat(payment): implement views for Payment model

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+
+from payments.models import Payment
+from payments.serializer import (
+    PaymentSerializer,
+    PaymentListSerializer,
+    PaymentDetailSerializer
+)
+
 
 # Create your views here.
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.select_related("borrowing__user", "borrowing__book")
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(borrowing__user=user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+
+        return PaymentSerializer
